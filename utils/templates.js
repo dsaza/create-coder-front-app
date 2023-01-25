f2ee34400c9583f0a1bdf5d978de015f012a92be
dsaza <dayrondaza05@gmail.com>
2023-01-25 15:57:07 -0500

    Update name to package.json in template copy

--- utils/templates.js
+++ utils/templates.js
@@ -13,6 +13,10 @@ export function templateCopyists(type, _path, name) {
 
 		const gitIgnoreFile = path.join(_path, './_gitignore')
 		fs.renameSync(gitIgnoreFile, gitIgnoreFile.replace('_gitignore', '.gitignore'))
+
+		const packageJsonFile = path.join(_path, './package.json')
+		const packageText = fs.readFileSync(packageJsonFile, 'utf8')
+		fs.writeFileSync(packageJsonFile, packageText.replace('name-project', name), null, 2)
 		
 		return true
 	} catch (error) {
