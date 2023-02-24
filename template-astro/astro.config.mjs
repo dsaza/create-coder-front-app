884f5a763a29399c180b66d41450ab428e19092b
dsaza <dayrondaza05@gmail.com>
2023-02-24 09:10:39 -0500

    Remove react from astro config in template-astro

--- template-astro/astro.config.mjs
+++ template-astro/astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import critters from 'astro-critters';
 import prefetch from '@astrojs/prefetch';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), critters(), prefetch(), react()]
+  integrations: [tailwind(), critters(), prefetch()]
 });
