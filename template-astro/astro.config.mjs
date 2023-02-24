a2dc8b27eb66858efda05da4a074ec5e063cf1e8
dsaza <dayrondaza05@gmail.com>
2023-02-24 09:28:25 -0500

    Update dependencies to template-astro

--- template-astro/astro.config.mjs
+++ template-astro/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import critters from 'astro-critters';
 import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), critters(), prefetch()]
+  integrations: [tailwind(), prefetch()]
 });
